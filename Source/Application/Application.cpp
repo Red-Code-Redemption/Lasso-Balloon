6f3fe27d816146c93e0b749c8fa5cd1d59cce145
RahulM-3 <rahulcode97@gmail.com>
2024-11-21 17:42:17 +0530

    added Initial Ascend, Ideal stay, Ascend control & Descend Control

--- Source/Application/Application.cpp
+++ Source/Application/Application.cpp
@@ -3,6 +3,11 @@
 #include <iostream>
 #include <vector>
 
+void LassoBaloon()
+{
+
+}
+
 void Application::Initialize(HMODULE _Module)
 {
 
@@ -12,8 +17,11 @@ void Application::Initialize(HMODULE _Module)
         {
             bool IsModActive = false;
             Actor PlayerActor = 0;
-            float Targetheight = 50;
-            bool HeightReached = false;
+
+            float MaxHeight = 50;
+            float CurrentHeight = 0;
+            int UpDownMotion = 1;
+
             Actor PrevTargetedActor = 0;
             Vector3 PrevTargetedActorLastCoords = { -1, -1, -1 };
 
@@ -35,70 +43,90 @@ void Application::Initialize(HMODULE _Module)
                         UNSORTED::PRINT_OBJECTIVE_B("<red>Lasso Baloon Deactive\n", 1.0f, true, 1, 0, 0, 0, 0);
                     }
                 }
-                UNSORTED::HUD_CLEAR_OBJECTIVE_QUEUE();
-                UNSORTED::PRINT_OBJECTIVE_B("WORKING", 1.0f, true, 1, 0, 0, 0, 0);
                 if (IsModActive)
                 {
-                    Actor TargetActor = UNSORTED::GET_LASSO_TARGET(PlayerActor);
                     Vector3 PlayerCoords = UNSORTED::GET_POSITION(PlayerActor);
+                    Actor TargetActor = UNSORTED::GET_LASSO_TARGET(PlayerActor);
                     Vector3 TargetActorCoords = UNSORTED::GET_POSITION(TargetActor);
+                    CurrentHeight = TargetActorCoords.y;
 
+                    PrevTargetedActor = TargetActor;
 
-                    if (TargetActor)
+                    UNSORTED::SET_ACTOR_INVULNERABILITY(PlayerActor, true);
+                    UNSORTED::SET_ACTOR_INVULNERABILITY(TargetActor, true);
+
+                    // Initial Ascend - TODO
+                    while (UNSORTED::IS_ACTOR_ON_GROUND(PlayerActor) && UNSORTED::GET_LASSO_TARGET(PlayerActor))
                     {
-                        PrevTargetedActor = TargetActor;
-
-                        while (!HeightReached && UNSORTED::GET_LASSO_TARGET(PlayerActor))
+                        if (!UNSORTED::IS_ACTOR_ON_GROUND(PlayerActor))
                         {
-                            // Come to Actor Z
-                            if (TargetActorCoords.z < PlayerCoords.z)
-                            {
-                                TargetActorCoords.z += 0.03;
-                            }
-                            if (TargetActorCoords.z < PlayerCoords.z)
-                            {
-                                TargetActorCoords.z -= 0.03;
-                            }
-
-                            // Come to Actor X
-                            if (TargetActorCoords.x > PlayerCoords.x)
-                            {
-                                TargetActorCoords.x -= 0.03;
-                            }
-                            if (TargetActorCoords.x < PlayerCoords.x)
-                            {
-                                TargetActorCoords.x += 0.03;
-                            }
-
-                            if (TargetActorCoords.y >= Targetheight)
-                            {
-                                HeightReached = true;
-                                break;
-                            }
-
-                            // Ascend Targeted NPC
-                            TargetActorCoords.y += 0.05;
-                            UNSORTED::TELEPORT_ACTOR(TargetActor, &TargetActorCoords, false, false, false);
-                            UNSORTED::WAIT(0);
+                            UNSORTED::TELEPORT_ACTOR(PlayerActor, &PlayerCoords, false, false, false);
+                            break;
                         }
-                        if(HeightReached)
-                        {
-                            // Maintain the Height
-                            TargetActorCoords.y = Targetheight;
-                            UNSORTED::TELEPORT_ACTOR(TargetActor, &TargetActorCoords, false, false, false);
-                        }
+                        TargetActorCoords.y += 0.05;
+                        PlayerCoords = UNSORTED::GET_POSITION(PlayerActor);
+                        UNSORTED::TELEPORT_ACTOR(TargetActor, &TargetActorCoords, false, false, false);
                         PrevTargetedActorLastCoords = TargetActorCoords;
+                        CurrentHeight = TargetActorCoords.y;
+                        UNSORTED::WAIT(0);
                     }
-                    else
+
+                    // Ascend Targeted NPC
+                    while(Input::IsKeyPressed(KEY_SPACE) && UNSORTED::GET_LASSO_TARGET(PlayerActor))
+                    {
+                        if (TargetActorCoords.y >= MaxHeight)
+                        {
+                            break;
+                        }
+                        TargetActorCoords.y += 0.05;
+                        PlayerCoords = UNSORTED::GET_POSITION(PlayerActor);
+                        UNSORTED::TELEPORT_ACTOR(TargetActor, &TargetActorCoords, false, false, false);
+                        PrevTargetedActorLastCoords = TargetActorCoords;
+                        CurrentHeight = TargetActorCoords.y;
+                        UNSORTED::WAIT(0);
+                    }
+
+                    // Descend Targeted NPC
+                    while(Input::IsKeyPressed(KEY_LEFT_ALT) && UNSORTED::GET_LASSO_TARGET(PlayerActor))
+                    {
+                        if (UNSORTED::IS_ACTOR_ON_GROUND(PlayerActor))
+                        {
+                            UNSORTED::TELEPORT_ACTOR(PlayerActor, &PlayerCoords, false, false, false);
+                            break;
+                        }
+
+                        TargetActorCoords.y -= 0.05;
+                        UNSORTED::TELEPORT_ACTOR(TargetActor, &TargetActorCoords, false, false, false);
+
+                        PrevTargetedActorLastCoords = TargetActorCoords;
+                        CurrentHeight = TargetActorCoords.y;
+                        UNSORTED::WAIT(0);
+                    }
+
+                    // Ideal Float
+                    while (UNSORTED::GET_LASSO_TARGET(PlayerActor))
+                    {
+                        if ((Input::IsKeyPressed(KEY_LEFT_ALT) && !UNSORTED::IS_ACTOR_ON_GROUND(PlayerActor)) || Input::IsKeyPressed(KEY_SPACE))
+                        {
+                            break;
+                        }
+                        TargetActorCoords.y = CurrentHeight;
+                        UNSORTED::TELEPORT_ACTOR(TargetActor, &TargetActorCoords, false, false, false);
+                        UNSORTED::WAIT(0);
+                    }
+
+                    if (!UNSORTED::GET_LASSO_TARGET(PlayerActor))
                     {
                         while (!UNSORTED::IS_ACTOR_ON_GROUND(PlayerActor))
                         {
-                            HEALTH::SET_ACTOR_HEALTH(PrevTargetedActor, 100);
                             UNSORTED::SET_OBJECT_POSITION(PrevTargetedActor, Vector2{ PrevTargetedActorLastCoords.x, PrevTargetedActorLastCoords.y }, PrevTargetedActorLastCoords.z);
                             PrevTargetedActorLastCoords = UNSORTED::GET_POSITION(PrevTargetedActor);
                             UNSORTED::WAIT(0);
                         }
-                        HeightReached = false;
+                        UNSORTED::SET_ACTOR_INVULNERABILITY(PlayerActor, false);
+                        UNSORTED::SET_ACTOR_INVULNERABILITY(PrevTargetedActor, true);
+
+                        CurrentHeight = 0;
                         PrevTargetedActor = 0;
                         PrevTargetedActorLastCoords = { -1, -1, -1 };
                     }
